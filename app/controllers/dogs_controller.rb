b7a3b2c63ac7fceb732e6a3be7835371be22013c
dev <sr.engineer0226@outlook.com>
2024-01-12 10:12:59 -0800

    update dogs controller to work with service and render results

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,7 +6,20 @@ class DogsController < ApplicationController
   def create
     @dog = Dog.new(dog_params)
 
-    @dog.save
+    if @dog.save
+      result = DogApiService.fetch_breed_image(@dog.breed_name)
+
+      if result.success
+        # Respond with TurboStreams to update the UI
+        render turbo_stream: turbo_stream.replace('dog_image', partial: 'dogs/image', locals: { breed_image: result.data })
+      else
+        @dog.errors.add(:breed_name, result.error_message)
+        render turbo_stream: turbo_stream.replace('dog_image', partial: 'dogs/error_messages', locals: { errors: @dog.errors })
+      end
+    else
+      # Handle validation errors
+      render :new
+    end
   end
 
   private
---
+++ app/services/dog_api_service.rb
@@ -0,0 +1,31 @@
+class DogApiService
+  BASE_URL = 'https://dog.ceo/api'
+
+  Result = Struct.new(:success, :data, :error_message)
+
+  def self.fetch_breed_image(breed_name)
+    response = Faraday.get("#{BASE_URL}/breed/#{breed_name}/images/random")
+    json = JSON.parse(response.body)
+
+    if response.success? && json['status'] == 'success'
+      Result.new(true, json['message'], nil)
+    else
+      Result.new(false, nil, "Breed not found: #{breed_name}. Possible breeds: #{fetch_available_breeds.data&.join(', ')}")
+    end
+  rescue Faraday::Error => e
+    Result.new(false, nil, "Error connecting to Dog API: #{e.message}")
+  end
+
+  def self.fetch_available_breeds
+    response = Faraday.get("#{BASE_URL}/breeds/list/all")
+    json = JSON.parse(response.body)
+
+    if response.success? && json['message'].is_a?(Array)
+      Result.new(true, json['message'].keys, nil)
+    else
+      Result.new(false, nil, "Error connecting to Dog API")
+    end
+  rescue Faraday::Error => e
+    Result.new(false, nil, "Error connecting to Dog API: #{e.message}")
+  end
+end
